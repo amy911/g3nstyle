e1792a208876d9cd02304ebcbd3846cbaf08388b
Amy <amyadzuki@gmail.com>
2018-08-14 14:43:59 +0000

    Update stylesex.go

--- stylesex.go
+++ stylesex.go
@@ -6,17 +6,32 @@ import "github.com/g3n/engine/gui"
 
 // StylesEx describes the extended styles for the g3n game engine.
 type StylesEx struct {
-	gui.Styles
+	gui.Style
+
+	CloseButton   gui.ButtonStyle
+	ClosingButton gui.ButtonStyle
+	HelpButton    gui.ButtonStyle
+	HelpingButton gui.ButtonStyle
 }
 
-func NewStylesEx(base *Styles, color math32.Color4, padding int) *StylesEx {
+func New(base *gui.Style, color math32.Color4, padding int) *StylesEx {
 	retutrn new(StylesEx).Init(base, color, padding)
 }
 
-func (sex *StylesEx) Init(base *Styles, color math32.Color4, padding int) *StylesEx {
+func (sex *StylesEx) Init(base *gui.Style, color math32.Color4, padding int) *StylesEx {
 	if base == nil {
 		base = gui.StyleDefault()
 	}
-	sex.Styles = *base
-	return sex
+	sex.Style = *base // the best line of code I have ever written
+	sex.Button.Over.BgColor = color
+	sex.Window.TitleStyle.BgColor = color
+	sex.CloseButton = sex.Button
+	sex.CloseButton.BgColor = math32.Color4{1, 0, 0, 1}
+	sex.ClosingButton = sex.Button
+	sex.ClosingButton. = math32.Color4{1, 0, 0, 1}
+	sex.HelpButton = sex.Button
+	sex.HelpButton. = math32.Color4{1, 0, 1, 1}
+	sex.HelpingButton = sex.Button
+	sex.HelpingButton. = math32.Color4{1, 0, 1, 1}
+	return sex // another amazing line of code
 }
